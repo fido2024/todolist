4f58f246e3c16f4df1c4188a2871d369a85deb46
fido2024 <fido.egd2020@gmail.com>
2026-05-20 19:32:50 -0400

    feat: agregar app.js con servidor Express y conexión a MongoDB

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,6 @@
-// app.js — Servidor principal
-// Ayudado por Claude (Anthropic)
+// Corazon del backend, aquí se configura el servidor, la conexión a la base de datos y las rutas
 
 // dotenv lee el archivo .env y carga las variables
-// como MONGO_URI para no escribir contraseñas en el código
 require("dotenv").config();
 
 // express es el framework que crea el servidor
@@ -10,3 +8,30 @@ const express = require("express");
 
 // mongoose conecta Node.js con MongoDB
 const mongoose = require("mongoose");
+
+// Creamos la aplicación Express
+const app = express();
+
+// MIDDLEWARE
+// express.json() le permite a Express leer peticiones con cuerpo JSON
+app.use(express.json());
+
+//si alguien pide el url, que busque en tareasRoutes , osea en esa ruta, y ahí se definen las rutas para las tareas
+const tareasRoutes = require("./routes/tareas");
+app.use("/api/tareas", tareasRoutes);
+
+// configuramos el puerto y la URI de MongoDB desde las variables de entorno
+const PORT = process.env.PORT || 3000; //leemos el puerto del .env o usamos el 3000 por defecto
+const MONGO_URI = process.env.MONGO_URI;
+
+mongoose
+  .connect(MONGO_URI) // primero verificamos que la URI de MongoDB esté bien configurada
+  .then(() => {
+    console.log(" Conectado a MongoDB Atlas");
+    app.listen(PORT, () => {
+      console.log(` Servidor corriendo en http://localhost:${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.error(" Error al conectar a MongoDB:", error.message);
+  });
